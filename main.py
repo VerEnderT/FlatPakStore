203830b93718f455a1e4b75806b9f786ff61adfa
VerEnderT <chaosz932@gmail.com>
2021-11-16 15:40:04 +0100

    neu

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import sys
-import pathlib
+# import pathlib
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtCore import *
-from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
-from PyQt5.QtMultimediaWidgets import QVideoWidget
+# from PyQt5.QtMultimedia import QMediaContent
 
 
 class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menübars
     def __init__(self):
         super().__init__()
 
-        self.beschreibung1 = "Karts. Nitro. Action! \n\nSuperTuxKart ist ein 3D-Open-Source-Arcade-Racer mit einer " \
-                             "Vielzahl von Charakteren, Strecken und Spielmodi. \n" \
-                             "Im Story-Modus musst du dich dem bösen Nolok stellen und ihn" \
-                             "besiegen, um das Maskottchen-Königreich wieder sicher zu machen! Sie können alleine " \
-                             "gegen den Computer antreten, an mehreren Grand-Prix-Pokalen teilnehmen oder versuchen, " \
-                             "Ihre schnellste Zeit im Zeitfahrmodus zu schlagen. Sie können auch mit bis zu acht " \
-                             "Freunden auf einem einzigen Computer Rennen fahren oder kämpfen, in einem lokalen " \
-                             "Netzwerk spielen oder online mit anderen Spielern auf der ganzen Welt spielen. "
-        self.beschreibung2 = "0 A.D. (kurz für Jahr null Anno Domini) ist ein kontinuierlich weiterentwickeltes " \
-                             "freies Echtzeit-Strategiespiel von Wildfire Games. Zu den Schwerpunkten des Spiels " \
-                             "zählt der Aufbau einer Wirtschafts- und Kriegsproduktion zur anschließenden Bekämpfung " \
-                             "der Gegner. Die im Spiel enthaltenen Zivilisationen und Technologien sind in etwa von " \
-                             "der realen historischen Entwicklung der Antike in den Jahren 500 bis 1 vor Christus " \
-                             "beeinflusst. "
+        self.testbutton = "0"
+        self.awalt = 0
+        self.awneu = 0
+        self.gameslist = ["Super Tux Kart", "0 A.D."]
+        self.coverpm = ["./SuperTuxKart.jpg", "./nullad.jpg"]
+        self.videoadr = ["ef9vYcuEDL4", "D3vxXZygHIk"]
+        self.yt1 = "http://www.youtube.com/embed/"
+        self.yt2 = "?autoplay=1&showinfo=0&controls=0&loop=1&playlist="
+        self.fileurl = self.yt1 + self.videoadr[0] + self.yt2 + self.videoadr[0]
+        self.beschreibung = ["Karts. Nitro. Action! \n\nSuperTuxKart ist ein 3D-Open-Source-Arcade-Racer mit einer " +
+                             "Vielzahl von Charakteren, Strecken und Spielmodi. \n" +
+                             "Im Story-Modus musst du dich dem bösen Nolok stellen und ihn" +
+                             "besiegen, um das Maskottchen-Königreich wieder sicher zu machen! Sie können alleine " +
+                             "gegen den Computer antreten, an mehreren Grand-Prix-Pokalen teilnehmen oder versuchen, " +
+                             "Ihre schnellste Zeit im Zeitfahrmodus zu schlagen. Sie können auch mit bis zu acht " +
+                             "Freunden auf einem einzigen Computer Rennen fahren oder kämpfen, in einem lokalen " +
+                             "Netzwerk spielen oder online mit anderen Spielern auf der ganzen Welt spielen. ",
+                             "0 A.D. (kurz für Jahr null Anno Domini) ist ein kontinuierlich weiterentwickeltes " +
+                             "freies Echtzeit-Strategiespiel von Wildfire Games. Zu den Schwerpunkten des Spiels " +
+                             "zählt der Aufbau einer Wirtschafts- und Kriegsproduktion zur anschließenden Bekämpfung " +
+                             "der Gegner. Die im Spiel enthaltenen Zivilisationen und Technologien sind in etwa von " +
+                             "der realen historischen Entwicklung der Antike in den Jahren 500 bis 1 vor Christus " +
+                             "beeinflusst. "]
 
         # Hintergrund
         image = QPixmap("./background.gif")
@@ -35,18 +44,13 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menübars
         background.setScaledContents(True)
         background.move(0, 0)
 
-        # Media player
-        filename = str(pathlib.Path(__file__).parent.resolve()) + "/SuperTuxKart.mp4"
-        self.videoWidget = QVideoWidget(self)
-        self.videoWidget.setMinimumWidth(400)
-        self.videoWidget.setMinimumHeight(225)
-        self.videoWidget.move(185, 20)
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
-        self.mediaPlayer.setVideoOutput(self.videoWidget)
-        self.mediaPlayer.play()
-        self.mediaPlayer.setVolume(0)
-        self.mediaPlayer.stateChanged.connect(self.play)
+        # webkit test
+        self.webview = QWebEngineView(self)
+        self.webview.setUrl(QUrl(self.fileurl))
+        self.webview.setMinimumWidth(400)
+        self.webview.setMinimumHeight(225)
+        self.webview.move(185, 20)
+        self.webview.settings()
 
         # Cover
         image1 = QPixmap("./SuperTuxKart.jpg")
@@ -75,7 +79,7 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menübars
         self.btext.setReadOnly(True)
         self.btext.setMinimumWidth(430)
         self.btext.setMinimumHeight(280)
-        self.btext.setText(self.beschreibung1)
+        self.btext.setText(self.beschreibung[1])
         self.btext.move(170, 300)
         self.btext.setStyleSheet("background: rgba(0, 0, 0, 140); color: #ffffff;")
 
@@ -83,7 +87,8 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menübars
         self.btn_install = QPushButton(self)
         self.btn_install.setText("Installieren")
         self.btn_install.move(700, 500)
-        self.btn_install.clicked.connect(self.knopf)
+        self.btn_install.setObjectName("2")
+        # self.btn_install.clicked.connect(self.knopf)
         self.btn_install.setMinimumWidth(200)
         self.btn_install.setMinimumHeight(50)
         self.btn_install.setStyleSheet(
@@ -93,49 +98,40 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menübars
             "font-size: 35px;" +
             "color: #ffffff;"
         )
+        self.count = 0
 
-        # SuperTuxKart
-        self.stk = QPushButton(self)
-        self.stk.setText("SuperTuxKart")
-        self.stk.move(50, 150)
-        self.stk.clicked.connect(self.cgame0)
-
-        # 0 A.D.
-        self.nullad = QPushButton(self)
-        self.nullad.setText("0 A.D.")
-        self.nullad.move(50, 180)
-        self.nullad.clicked.connect(self.cgame1)
+        for i in self.gameslist:
+            self.gamebtn = QPushButton(self)
+            self.gamebtn.setText(self.gameslist[self.count])
+            self.gamebtn.move(50, 150 + self.count * 30)
+            self.aobjectname = str(self.count)
+            self.gamebtn.setObjectName(self.aobjectname)
+            self.count = +1
+            self.gamebtn.clicked.connect(self.awknopf)
+            print(i)
 
 # ---------------------------------------------------------------------------------------------
 
         # konfiguration Fenster und zeigen
-        self.setGeometry(50, 50, 1000, 600)  # x-pos, y-pos, breite, höhe
+        self.setGeometry(1280 + 50, 50, 1000, 600)  # x-pos, y-pos, breite, höhe
         self.setWindowTitle("VerFlatpakT")  # Title name
         self.setWindowIcon(QIcon("VETlogo.png"))  # Datei für das logo des programms
         self.setFixedSize(1000, 600)  # fixe größe einstellen
         self.show()  # Fenster anzeigen
 
-    def knopf(self):
-        print(self.mediaPlayer.isAvailable())
-        print(self.mediaPlayer.state())
-        print(self.mediaPlayer.errorString())
+    def awknopf(self):
+        e = self.sender()
+        print(e.objectName())
+        self.awalt = self.awneu
+        self.awneu = int(e.objectName())
+        self.gamechange()
 
-    def play(self):
-        self.mediaPlayer.play()
-
-    def cgame0(self):
-        self.cover.setPixmap(QPixmap("./SuperTuxKart.jpg"))
-        self.gamename.setText("Super TuX Kart")
-        self.btext.setText(self.beschreibung1)
-        filename = str(pathlib.Path(__file__).parent.resolve()) + "/SuperTuxKart.mp4"
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
-
-    def cgame1(self):
-        self.cover.setPixmap(QPixmap("./nullad.jpg"))
-        self.gamename.setText("0 A.D.")
-        self.btext.setText(self.beschreibung2)
-        filename = str(pathlib.Path(__file__).parent.resolve()) + "/nullAD.webm"
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(filename)))
+    def gamechange(self):
+        self.gamename.setText(self.gameslist[self.awneu])
+        self.cover.setPixmap(QPixmap(self.coverpm[self.awneu]))
+        self.btext.setText(self.beschreibung[self.awneu])
+        self.fileurl = self.yt1 + self.videoadr[self.awneu] + self.yt2 + self.videoadr[self.awneu]
+        self.webview.setUrl(QUrl(self.fileurl))
 
 
 app = QApplication(sys.argv)
