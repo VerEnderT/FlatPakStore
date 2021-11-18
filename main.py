2c44489b68971c5e024ca81b8368278578272bc0
VerEnderT <chaosz932@gmail.com>
2021-11-18 21:05:39 +0100

    update

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from PyQt5.QtCore import *
 class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
     def __init__(self):
         super().__init__()
+        self.count = 0
+        self.btn_app = QPushButton(self)
+        self.btn_cat = QPushButton(self)
+        self.aobjectname = ""
+        self.cobjectname = ""
+        self.button = []
         self.appcategorie = []
         self.appcpath = []
         self.apppage = 0
@@ -30,6 +36,21 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
         cpath = os.path.dirname(os.path.abspath(__file__))
         self.catseinlesen()
         self.appseinlesen()
+
+# ---------------- AppButton ---------------------------------------------------
+
+        self.btn_app0 = QPushButton(self)
+        self.btn_app1 = QPushButton(self)
+        self.btn_app2 = QPushButton(self)
+        self.btn_app3 = QPushButton(self)
+        self.btn_app4 = QPushButton(self)
+        self.btn_app5 = QPushButton(self)
+        self.btn_app6 = QPushButton(self)
+        self.btn_app7 = QPushButton(self)
+        self.btn_app8 = QPushButton(self)
+        self.btn_app9 = QPushButton(self)
+
+# ---------------------------------------------------------------------------
 
         # Hintergrund
         image = QPixmap(cpath + "/background.gif")
@@ -160,7 +181,7 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
         self.stAnzeige.setMinimumHeight(50)
         self.stAnzeige.setText(self.appcategorie[self.appcselect])
         self.stAnzeige.move(20, 50)
-        #self.stAnzeige.hide()
+        # self.stAnzeige.hide()
         self.stAnzeige.setAlignment(Qt.AlignCenter)
         self.stAnzeige.setStyleSheet(
             "background: rgba(0, 200, 0, 150);" +
@@ -188,6 +209,28 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
         self.awneu = int(e.objectName())
         self.appchange()
 
+    def cwknopf(self):
+        e = self.sender()
+        print("knopf " + e.objectName())
+        self.appcselect = int(e.objectName())
+        text = self.appcategorie[self.appcselect]
+        self.stAnzeige.setText(text)
+        self.btn_app.setUpdatesEnabled(True)
+        self.btn_app0.close()
+        self.btn_app1.close()
+        self.btn_app2.close()
+        self.btn_app3.close()
+        self.btn_app4.close()
+        self.btn_app5.close()
+        self.btn_app6.close()
+        self.btn_app7.close()
+        self.btn_app8.close()
+        self.btn_app9.close()
+        self.awneu = 0
+        self.appseinlesen()
+        self.appbtnmake()
+        self.appchange()
+
     def appchange(self):
         self.appname.setText(self.applist[self.awneu])
         self.cover.setPixmap(QPixmap(self.coverpm[self.awneu]))
@@ -211,15 +254,12 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
             self.btn_deinstall.hide()
 
     def installieren(self):
-        self.stAnzeige.show()
         self.stAnzeige.setText("es wird installiert")
         s = subprocess.Popen(['flatpak', 'install', self.appcom[self.awneu], '-y'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # stdout, stderr = s.communicate()
         print("wird installiert")
         self.update_btn()
-        self.stAnzeige.hide()
-
     def deinstallieren(self):
         self.stAnzeige.setText("Es wird deinstalliert...")
         self.stAnzeige.show()
@@ -228,7 +268,6 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
         # stdout=s.communicate()
         print("wird de-installiert")
         self.update_btn()
-        self.stAnzeige.hide()
 
     def starten(self):
         try:
@@ -244,7 +283,7 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
         if self.i >= 100:
             # self.timer.stop()
             self.i = 0
-            self.update_btn()
+            #self.update_btn()
         self.i = self.i + 1
         self.stimes.setValue(int(self.i))
 
@@ -259,6 +298,11 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
         print(catpath)
         file1 = open(catpath, 'r')
         count = 0
+        self.applist.clear()
+        self.coverpm.clear()
+        self.videoadr.clear()
+        self.beschreibung.clear()
+        self.appcom.clear()
         while True:
             line = file1.readline()
             if not line:
@@ -288,21 +332,137 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
             print("Line{}: {}".format(count, line.strip()))
 
             count += 1
-        self.appmenge = count
+        self.appmenge = count - 1
         file1.close()
 
         # Applist Buttons
-    def appbtnmake(self):
+    def appbtn_make(self):
         self.count = 0
         for i in self.applist:
             self.btn_app = QPushButton(self)
-            self.btn_app.setText(self.applist[self.count])
-            self.btn_app.move(50, 130 + self.count * 35)
             self.aobjectname = str(self.count)
+            self.btn_app.setUpdatesEnabled(True)
             self.btn_app.setObjectName(self.aobjectname)
+            self.btn_app.setText(self.applist[self.count])
             self.btn_app.clicked.connect(self.awknopf)
+            self.btn_app.move(50, 130 + self.count * 35)
             self.count = self.count + 1
             print(i)
+
+    def appbtnmake(self):
+        print(self.appcategorie[self.appcselect])
+
+        self.btn_app0 = QPushButton(self)
+        self.btn_app0.move(50, 130 + 0 * 35)
+        self.btn_app0.setObjectName("0")
+        self.btn_app0.clicked.connect(self.awknopf)
+        self.btn_app1 = QPushButton(self)
+        self.btn_app1.move(50, 130 + 1 * 35)
+        self.btn_app1.setObjectName("1")
+        self.btn_app1.clicked.connect(self.awknopf)
+        self.btn_app2 = QPushButton(self)
+        self.btn_app2.move(50, 130 + 2 * 35)
+        self.btn_app2.setObjectName("2")
+        self.btn_app2.clicked.connect(self.awknopf)
+        self.btn_app3 = QPushButton(self)
+        self.btn_app3.move(50, 130 + 3 * 35)
+        self.btn_app3.setObjectName("3")
+        self.btn_app3.clicked.connect(self.awknopf)
+        self.btn_app4 = QPushButton(self)
+        self.btn_app4.move(50, 130 + 4 * 35)
+        self.btn_app4.setObjectName("4")
+        self.btn_app4.clicked.connect(self.awknopf)
+        self.btn_app5 = QPushButton(self)
+        self.btn_app5.move(50, 130 + 5 * 35)
+        self.btn_app5.setObjectName("5")
+        self.btn_app5.clicked.connect(self.awknopf)
+        self.btn_app6 = QPushButton(self)
+        self.btn_app6.move(50, 130 + 6 * 35)
+        self.btn_app6.setObjectName("6")
+        self.btn_app6.clicked.connect(self.awknopf)
+        self.btn_app7 = QPushButton(self)
+        self.btn_app7.move(50, 130 + 7 * 35)
+        self.btn_app7.setObjectName("7")
+        self.btn_app7.clicked.connect(self.awknopf)
+        self.btn_app8 = QPushButton(self)
+        self.btn_app8.move(50, 130 + 8 * 35)
+        self.btn_app8.setObjectName("8")
+        self.btn_app8.clicked.connect(self.awknopf)
+        self.btn_app9 = QPushButton(self)
+        self.btn_app9.move(50, 130 + 9 * 35)
+        self.btn_app9.setObjectName("9")
+        self.btn_app9.clicked.connect(self.awknopf)
+
+        self.btn_app0.setText(self.applist[0])
+        self.btn_app0.show()
+
+        if self.appmenge >= self.apppage * 10 + 1:
+            self.btn_app1.setText(self.applist[1])
+            self.btn_app1.show()
+            print("an" + self.applist[1])
+        else:
+            self.btn_app1.hide()
+
+        if self.appmenge >= self.apppage * 10 + 2:
+            self.btn_app2.setText(self.applist[2])
+            self.btn_app2.show()
+            print("an")
+        else:
+            self.btn_app2.hide()
+
+        if self.appmenge >= self.apppage * 10 + 3:
+            self.btn_app3.setText(self.applist[3])
+            self.btn_app3.show()
+            print("an")
+        else:
+            self.btn_app3.hide()
+
+        if self.appmenge >= self.apppage * 10 + 4:
+            self.btn_app4.setText(self.applist[4])
+            self.btn_app4.show()
+            print("an")
+        else:
+            self.btn_app4.hide()
+
+        if self.appmenge >= self.apppage * 10 + 5:
+            self.btn_app5.setText(self.applist[5])
+            self.btn_app5.show()
+            print("an")
+        else:
+            self.btn_app5.hide()
+
+        if self.appmenge >= self.apppage * 10 + 6:
+            self.btn_app6.setText(self.applist[6])
+            self.btn_app6.show()
+            print("an")
+        else:
+            self.btn_app6.hide()
+
+        if self.appmenge >= self.apppage * 10 + 7:
+            print(str(self.appmenge)+"-"+str(self.apppage * 10 +7))
+            self.btn_app7.setText(self.applist[7])
+            self.btn_app7.show()
+            print("an")
+        else:
+            self.btn_app7.hide()
+
+        if self.appmenge >= self.apppage * 10 + 8:
+            print(str(self.appmenge)+"-"+str(self.apppage * 10 +8))
+            self.btn_app8.setText(self.applist[8])
+            self.btn_app8.show()
+            print("an")
+        else:
+            self.btn_app8.hide()
+
+        if self.appmenge >= self.apppage * 10 + 9:
+            print("wtf" + str(self.appmenge)+"-"+str(self.apppage * 10 +9))
+            self.btn_app9.setText(self.applist[9])
+            self.btn_app9.show()
+            print("an")
+        else:
+            self.btn_app9.hide()
+
+
 
     def catseinlesen(self):
         cpath = os.path.dirname(os.path.abspath(__file__))
@@ -325,7 +485,6 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
             count += 1
         file1.close()
 
-
     # Applist Buttons
     def catbtnmake(self):
         self.count = 0
@@ -334,10 +493,10 @@ class qFenster(QMainWindow):   # QMainWindow oder Qwidget für menuebars
             self.btn_cat.setText(self.appcategorie[self.count])
             self.btn_cat.move(10+ self.count * 100, 5 )
             self.cobjectname = str(self.count)
-            self.btn_cat.setObjectName(self.aobjectname)
-            #self.btn_cat.clicked.connect(self.cwknopf)
+            self.btn_cat.setObjectName(self.cobjectname)
+            self.btn_cat.clicked.connect(self.cwknopf)
+            print("catcount" + str(self.count) + " - " + i)
             self.count = self.count + 1
-            print(i)
 # --------------------------------------------------------------------------------------------
 
 
